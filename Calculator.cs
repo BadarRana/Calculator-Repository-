9eb4ae985b0f0cce818cc87877971211c2547488
BadarRana <24-st-015@students.ptut.edu.pk>
2025-03-06 14:01:02 +0500

    Update multiplication Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -5,5 +5,12 @@ class program
   {
     Console.Writeline("Rana Badar Munir");
     Console.Writeline("24-ST-015");
+    Console.Write("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Product: {a * b}");
   }
 }
